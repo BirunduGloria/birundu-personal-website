4997df5c1eb9d47d191cb6c90b064c1050754559
Gloria Birundu <birundugloria@gmail.com>
2025-06-01 21:53:16 +0300

    this is my first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const askButton = document.getElementById('ask-button');
+
+function handleAskQuestion() {
+  alert("By day, I design pixels. By night, I dodge bullets with Batman.Balance is my superpower")
+}
+
+askButton.addEventListener('click', handleAskQuestion);
